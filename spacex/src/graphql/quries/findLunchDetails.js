0a33ec0690ecbbf11e91b59838e2c1b1d3e6a544
Vikas Mishra <vm66353@gmail.com>
2022-07-28 17:13:03 +0530

    "version 1"

--- spacex/src/graphql/quries/findLunchDetails.js
+++ spacex/src/graphql/quries/findLunchDetails.js
@@ -1,40 +1,40 @@
 import { gql } from "@apollo/client";
 
 export const FindLaunchDetail = gql`
-query launch($id: ID!) {
-  launch(id: $id) {
-    mission_name
-    launch_site {
-      site_name
-    }
-    rocket {
-      rocket_name
-      second_stage {
-        payloads {
-          orbit
-          nationality
-          payload_type
-          manufacturer
-        }
+  query launch($id: ID!) {
+    launch(id: $id) {
+      mission_name
+      launch_site {
+        site_name
       }
-      rocket_type
       rocket {
-        description
-        type
-      }
-      first_stage {
-        cores {
-          flight
+        rocket_name
+        second_stage {
+          payloads {
+            orbit
+            nationality
+            payload_type
+            manufacturer
+          }
+        }
+        rocket_type
+        rocket {
+          description
+          type
+        }
+        first_stage {
+          cores {
+            flight
+          }
         }
       }
+      links {
+        wikipedia
+        video_link
+      }
+      launch_date_local
+      launch_success
+      upcoming
     }
-    links {
-      wikipedia
-      video_link
-    }
-    launch_date_local
-    launch_success
-    upcoming
   }
-}
 `;
--- spacex/src/typescript/findLaunchDetailsTS.ts
+++ spacex/src/typescript/findLaunchDetailsTS.ts
@@ -1,5 +1,5 @@
-export interface id{
-  id:number
+export interface id {
+  id: number;
 }
 
 // export interface Root {
@@ -7,56 +7,56 @@ export interface id{
 // }
 
 export interface Root {
-  launch: Launch
+  launch: Launch;
 }
 
 export interface Launch {
-  mission_name: string
-  launch_site: LaunchSite
-  rocket: Rocket
-  links: Links
-  launch_date_local: string
-  launch_success:boolean
-  upcoming:boolean
+  mission_name: string;
+  launch_site: LaunchSite;
+  rocket: Rocket;
+  links: Links;
+  launch_date_local: string;
+  launch_success: boolean;
+  upcoming: boolean;
 }
 
 export interface LaunchSite {
-  site_name: string
+  site_name: string;
 }
 
 export interface Rocket {
-  rocket_name: string
-  second_stage: SecondStage
-  rocket_type: string
-  rocket: Rocket2
-  first_stage: FirstStage
+  rocket_name: string;
+  second_stage: SecondStage;
+  rocket_type: string;
+  rocket: Rocket2;
+  first_stage: FirstStage;
 }
 
 export interface SecondStage {
-  payloads: Payload[]
+  payloads: Payload[];
 }
 
 export interface Payload {
-  orbit: string
-  nationality: string
-  payload_type: string
-  manufacturer?: string
+  orbit: string;
+  nationality: string;
+  payload_type: string;
+  manufacturer?: string;
 }
 
 export interface Rocket2 {
-  description: string
-  type: string
+  description: string;
+  type: string;
 }
 
 export interface FirstStage {
-  cores: Core[]
+  cores: Core[];
 }
 
 export interface Core {
-  flight: number
+  flight: number;
 }
 
 export interface Links {
-  wikipedia: string
-  video_link: string
+  wikipedia: string;
+  video_link: string;
 }
